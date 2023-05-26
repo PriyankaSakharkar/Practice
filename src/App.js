1c54f083a42cb02dfaeef7d7f8c225963ec7e728
PriyankaSakharkar <priyanka@sakharkar>
2023-05-26 15:29:26 +0530

    2nd commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+function App() {
+  return (
+    <div>
+       <table>
+  <tr>
+    <th>Name</th>
+    <th>Collage</th>
+    <th>City</th>
+  </tr>
+  <tr>
+    <td>Priyanka Sakharkar</td>
+    <td>KDK</td>
+    <td>Nagpur</td>
+  </tr>
+  <tr>
+    <td>Neha Likhar</td>
+    <td>NIT</td>
+    <td>Nagpur</td>
+
+  </tr>
+  <tr>
+    <td>Vaishali Barai</td>
+    <td>Prerna</td>
+    <td>Nagpur</td>
+    
+  </tr>
+</table>
+    </div>
+  );
+}
+
+export default App;
